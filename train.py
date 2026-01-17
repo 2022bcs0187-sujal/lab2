416780814de13e25944df7a01e2ba8ec6513b015
2022bcs0187-sujal <sujal20temp@gmail.com>
2026-01-17 23:02:53 +0530

    Baseline model: Random Forest, scaler, test split 0.2, no. of trees:200

--- train.py
+++ train.py
@@ -4,10 +4,9 @@ import json
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error, r2_score
 import joblib
-
+from sklearn.ensemble import RandomForestRegressor
 # Paths
 DATA_PATH = "Dataset/winequality-red.csv"
 MODEL_PATH = "Output/model/model.joblib"
@@ -35,7 +34,14 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Model
-model = LinearRegression()
+model = RandomForestRegressor(
+    n_estimators=200,
+    max_depth=12,
+    min_samples_split=5,
+    min_samples_leaf=2,
+    random_state=42,
+    n_jobs=-1
+)
 model.fit(X_train, y_train)
 
 # Predictions
