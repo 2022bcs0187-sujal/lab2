46276bb27c3f7ae4791d43ec764fc11ae7a59bca
2022bcs0187-sujal <sujal20temp@gmail.com>
2026-03-15 00:36:12 +0530

    Experiment 4: Model: Ridge Regression, alpha=1.0

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import mean_squared_error, r2_score
 import joblib
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import Ridge
 # Paths
 DATA_PATH = "Dataset/winequality-red.csv"
 MODEL_PATH = "Output/model/model.joblib"
@@ -34,7 +34,7 @@ X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
 # Model
-model = LinearRegression()
+model = Ridge(alpha=1.0)
 model.fit(X_train, y_train)
 
 # Predictions
