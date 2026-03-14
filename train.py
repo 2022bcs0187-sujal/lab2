7b8e28d1ff28a0c124cc41efdc986e1c70a94796
2022bcs0187-sujal <sujal20temp@gmail.com>
2026-03-15 00:32:12 +0530

    Experiment 2: RandomForest depth 20

--- train.py
+++ train.py
@@ -35,10 +35,10 @@ X_test = scaler.transform(X_test)
 
 # Model
 model = RandomForestRegressor(
-    n_estimators=200,
-    max_depth=12,
-    min_samples_split=5,
-    min_samples_leaf=2,
+    n_estimators=300,
+    max_depth=20,
+    min_samples_split=2,
+    min_samples_leaf=1,
     random_state=42,
     n_jobs=-1
 )
